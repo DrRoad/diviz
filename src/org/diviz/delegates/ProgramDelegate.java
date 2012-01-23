5b89f132d03519c770b73a05c8cdcd4def66acd6
Sébastien Bigaret <sebastien.bigaret@telecom-bretagne.eu>
2012-01-23 11:15:14 +0100

    csvToXMCDA-performanceTable: replaced the J-MCDA version by PyXMCDA version.
    reason: J-MCDA csvToXMCDA-performanceTable is very strict when parsing
    the csv (accepts only comma separated values, literally).  It was
    easier to implement a more flexible version in python, hence the
    replacement.

--- src/org/diviz/delegates/ProgramDelegate.java
+++ src/org/diviz/delegates/ProgramDelegate.java
@@ -52,6 +52,12 @@ public class ProgramDelegate
 		}
 		else if ( "local".equals(provider) && "prg_generic".equals(name) )
 			name = "generic_program";
+		else if ( "J-MCDA".equals(provider) && "csvToXMCDA-performanceTable".equals(name) )
+		{
+			provider = "PyXMCDA";
+			warnings.add(XMLWarnings.PRG_DESC_REPLACED_BY_ALTERNATIVE,
+			             new String[] { "J-MCDA csvToXMCDA-performanceTable", "PyXMCDA csvToXMCDA-performanceTable" });
+		}
 		else
 			return null;
 
